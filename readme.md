e20d62bfdbd8b10dc9094ca1cb6dcc7c6a1d1a85
Your Name <you@example.com>
2023-10-04 22:34:56 +1000

    api final version

---
+++ readme.md
@@ -0,0 +1,2 @@
+1. docker-compose up (redis в docker - нужен docker)
+2. mvn spring-boot:run (нужен maven)
--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -18,14 +24,33 @@ public class HelloWorldApplication {
 
 @RestController
 class HelloWorldController {
-    @GetMapping("/")
-    public String helloWorld() {
+    @GetMapping()
+    public String mainRoute() {
         final Jedis redis = RedisService.getClient();
         redis.set("foo", "bar");
         final String bar = redis.get("foo");
         System.out.println(bar);
+        final String id = IdService.generate("null");
+        return id;
+    }
+    @GetMapping("/{id}")
+    public RedirectView handleR(@PathVariable String id) {
+        final Jedis redis = RedisService.getClient();
+        final String url = redis.get(id);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(url);
+        return redirectView;
 
-        return bar;
+    }
+    @PostMapping("/getId")
+    public String servise(@RequestBody final GetIdJson request) {
+        final String url = request.getUrl();
+        final String id = IdService.generate(url);
+        final Jedis redis = RedisService.getClient();
+        redis.set(id, url);
+        final String bar = redis.get(id);
+        System.out.println(bar);
+        return id;
     }
 }
 
@@ -36,5 +61,21 @@ class RedisService{
     }
     public static void declareClient(){
         resourse = new JedisPool("localhost", 1000).getResource();
+    }
+}
+
+class IdService {
+    public static String generate(String link){
+        String salt = "$2a$10$Tdc1oXEY8UPd87VC4kd8GO";
+        String hash = BCrypt.hashpw(link, salt);
+        String id = hash.substring(29, Math.min(hash.length(), 41));
+        return id;
+    }
+}
+
+class GetIdJson {
+    String url;
+    public String getUrl() {
+        return url;
     }
 }
